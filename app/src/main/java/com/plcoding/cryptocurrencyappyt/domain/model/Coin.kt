675d0b2aa0ebeb37c5ef28367a2a97440e6b07c8
Allef Sousa <allefsousa_1@hotmail.com>
2021-12-07 18:46:56 -0300

    Criando pacotes

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/model/Coin.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptocurrencyappyt.domain.model
+
+class Coin(
+    val id: String,
+    val isActive: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+)
