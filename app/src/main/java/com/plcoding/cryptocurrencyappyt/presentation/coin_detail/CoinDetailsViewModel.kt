c52e6e7010995c3477329d1e36a6e3b63cebc4df
Allef Sousa <allefsousa_1@hotmail.com>
2021-12-07 19:30:52 -0300

    Create viewmodel and state ui coins

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_detail/CoinDetailsViewModel.kt
@@ -0,0 +1,50 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_detail
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptocurrencyappyt.common.Constants
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.domain.use_case.get_coin.GetCoinUseCase
+import com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins.GetCoinsUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class CoinDetailsViewModel @Inject constructor(
+    private val getCoinUseCase: GetCoinUseCase,
+    private val savedSateHandler:SavedStateHandle
+) : ViewModel() {
+
+    private val _state = mutableStateOf(CoinDetailsState())
+    val state: State<CoinDetailsState> = _state
+
+    init {
+        savedSateHandler.get<String>(Constants.PARAM_COIN_ID)?.let { coinId ->
+            getCoin(coinId)
+        }
+    }
+
+    private fun getCoin(coinId:String) {
+        getCoinUseCase(coinId).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinDetailsState(coin = result.data)
+                }
+
+                is Resource.Error -> {
+                    _state.value = CoinDetailsState(error = result.message ?: "Um Erro aconteceu")
+                }
+
+                is Resource.Loading -> {
+                    _state.value = CoinDetailsState(isLoading = true)
+
+                }
+            }
+
+        }.launchIn(viewModelScope)
+    }
+
+}
